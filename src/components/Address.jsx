51dbb96062630ceab21164ddaa70c0521358129f
codemaster034 <codemaster034@gmail.com>
2022-01-25 04:12:54 +1000

    Change address

--- src/components/Address.jsx
+++ src/components/Address.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 const Address = () => {
   const contactInfo = {
-    address: "1-8-15 Aonocho, Nara City, Nara Prefecture 631-0841",
-    mail: "codemaster034@gmail.com",
+    address: "Aichi, Okazaki-shi, Kami Sato 444-2136",
+    mail: "ryo.kanazawa34@gmail.com",
     phone: "+81 50 5539 9613",
   };
   return (
